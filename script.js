2f1b064a7f7c35386d3346726bdf4ad4037088b0
JerBar92 <174113693+JerBar92@users.noreply.github.com>
2024-09-20 15:57:50 +0200

    Work for simple operations with 2 numbers

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 let result = 0;
 let operator = "";
-let tempNumbers = [];
+let operand = [];
+let tempNumber = "";
 
 const display = document.getElementById("onScreen");
+const btn = document.querySelectorAll(".number");
+const btn0 = document.getElementById("btn0");
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+const btn6 = document.getElementById("btn6");
+const btn7 = document.getElementById("btn7");
+const btn8 = document.getElementById("btn8");
+const btn9 = document.getElementById("btn9");
 const btnAdd = document.getElementById("btnAdd");
+const btnSub = document.getElementById("btnSub");
+const btnMult = document.getElementById("btnMult");
+const btnDiv = document.getElementById("btnDiv");
 const btnEq = document.getElementById("btnEq");
+const btnAc = document.getElementById("btnAc");
+const btnC = document.getElementById("btnC");
+const btnDel = document.getElementById("btnDel");
+const btnNeg = document.getElementById("btnNeg");
+const btnFloat = document.getElementById("btnFloat");
 
 function add(num1, num2) {
   return num1 + num2;
@@ -41,22 +59,96 @@ function operate(num1, num2) {
   }
 }
 
+function reset() {
+  result = 0;
+  operator = "";
+  operand = [];
+  tempNumber = "";
+  display.textContent = "0";
+}
+btn0.addEventListener("click", () => {
+  tempNumber += "0";
+  display.textContent = tempNumber;
+});
+
 btn1.addEventListener("click", () => {
-  display.textContent = "1";
-  tempNumbers.push(Number(display.textContent));
+  tempNumber += "1";
+  display.textContent = tempNumber;
 });
 
 btn2.addEventListener("click", () => {
-  display.textContent = "2";
-  tempNumbers.push(Number(display.textContent));
+  tempNumber += "2";
+  display.textContent = tempNumber;
+});
+
+btn3.addEventListener("click", () => {
+  tempNumber += "3";
+  display.textContent = tempNumber;
+});
+
+btn4.addEventListener("click", () => {
+  tempNumber += "4";
+  display.textContent = tempNumber;
+});
+
+btn5.addEventListener("click", () => {
+  tempNumber += "5";
+  display.textContent = tempNumber;
+});
+
+btn6.addEventListener("click", () => {
+  tempNumber += "6";
+  display.textContent = tempNumber;
+});
+
+btn7.addEventListener("click", () => {
+  tempNumber += "7";
+  display.textContent = tempNumber;
+});
+
+btn8.addEventListener("click", () => {
+  tempNumber += "8";
+  display.textContent = tempNumber;
+});
+
+btn9.addEventListener("click", () => {
+  tempNumber += "9";
+  display.textContent = tempNumber;
 });
 
 btnAdd.addEventListener("click", () => {
+  operand.push(Number(tempNumber));
   display.textContent = "+";
   operator = "+";
+  tempNumber = "";
+});
+
+btnSub.addEventListener("click", () => {
+  operand.push(Number(tempNumber));
+  display.textContent = "-";
+  operator = "-";
+  tempNumber = "";
+});
+
+btnMult.addEventListener("click", () => {
+  operand.push(Number(tempNumber));
+  display.textContent = "*";
+  operator = "*";
+  tempNumber = "";
+});
+
+btnDiv.addEventListener("click", () => {
+  operand.push(Number(tempNumber));
+  display.textContent = "/";
+  operator = "/";
+  tempNumber = "";
 });
 
 btnEq.addEventListener("click", () => {
-  operate(tempNumbers[0], tempNumbers[1]);
+  operand.push(Number(tempNumber));
+  operate(operand[0], operand[1]);
   display.textContent = result;
+  operand.splice(0, 2, result);
 });
+
+btnAc?.addEventListener("click", () => reset());
